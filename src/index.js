7ff71f9835fb3354500fac6d8683dea2125dbbe3
Jordanlabuchanan <125835811+Jordanlabuchanan@users.noreply.github.com>
2023-10-05 12:26:19 -0500

    hopefully final changes

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 
 
 document.addEventListener("DOMContentLoaded", () => {
-  let gamesData = []; // To store the original data
+  let gamesData = []; 
   
-  //fetch json data
+  
   fetch('http://localhost:3000/nintendoGames')
       .then(response => response.json())
       .then(data => {
-          gamesData = data; // Store the original data
+          gamesData = data; 
           
-          // Function that displays games forEach for array iteration
+          
           function displayGames(games) {
               const gameList = document.getElementById("game-list");
               gameList.innerHTML = "";
@@ -23,12 +23,12 @@ document.addEventListener("DOMContentLoaded", () => {
                       <p>Rating: ${game.rating}</p>
                   `;
 
-                  // Mouseover event listener 1
+                  
                   gameCard.addEventListener("mouseover", () => {
                       gameCard.style.backgroundColor = "White";
                   });
 
-                  // Remove the mouseover effect when the mouse leaves card
+                  
                   gameCard.addEventListener("mouseout", () => {
                       gameCard.style.backgroundColor = "";
                   });
@@ -37,16 +37,16 @@ document.addEventListener("DOMContentLoaded", () => {
               });
           }
 
-          // Display all games using array iteration
+          
           displayGames(gamesData);
 
-          // toggle event listener 2
+          
           const darkModeButton = document.getElementById("darkModeButton");
           darkModeButton.addEventListener("click", () => {
-              // Toggle dark mode state
+              
               darkMode = !darkMode;
 
-              // Apply dark or light mode styles to the body
+              
               const body = document.body;
               if (darkMode) {
                   body.classList.add("dark-mode");
@@ -55,17 +55,17 @@ document.addEventListener("DOMContentLoaded", () => {
               }
           });
 
-          // Filter button event listener 3
+          
           const filterButton = document.getElementById("filterButton");
           filterButton.addEventListener("click", () => {
               const ratingSelect = document.getElementById("ratingSelect");
               const selectedRating = ratingSelect.value;
 
               if (selectedRating === "all") {
-                  // Display all games using array iteration
+                  
                   displayGames(gamesData);
               } else {
-                  // Filter games by the rating using array iteration
+                  
                   const filteredGames = gamesData.filter(game => game.rating == selectedRating);
                   displayGames(filteredGames);
               }
