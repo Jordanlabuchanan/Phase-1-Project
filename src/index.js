d74fd9d5260e650f4c302906eabed0162647365f
Jordanlabuchanan <125835811+Jordanlabuchanan@users.noreply.github.com>
2023-12-07 11:39:39 -0600

    changes made

--- src/index.js
+++ src/index.js
@@ -1,78 +1,72 @@
 
 
 document.addEventListener("DOMContentLoaded", () => {
-  let gamesData = []; 
+    let gamesData = [];
   
-  
-  fetch('http://localhost:3000/nintendoGames')
+    fetch('http://localhost:3000/nintendoGames')
       .then(response => response.json())
       .then(data => {
-          gamesData = data; 
-          
-          
-          function displayGames(games) {
-              const gameList = document.getElementById("game-list");
-              gameList.innerHTML = "";
-
-              games.forEach((game) => {
-                  const gameCard = document.createElement("div");
-                  gameCard.classList.add("game-card");
-                  gameCard.innerHTML = `
-                      <img src="${game.image}" alt="${game.name}">
-                      <h2>${game.name}</h2>
-                      <p>Rating: ${game.rating}</p>
-                  `;
-
-                  
-                  gameCard.addEventListener("mouseover", () => {
-                      gameCard.style.backgroundColor = "White";
-                  });
-
-                  
-                  gameCard.addEventListener("mouseout", () => {
-                      gameCard.style.backgroundColor = "";
-                  });
-
-                  gameList.appendChild(gameCard);
-              });
+        gamesData = data;
+  
+        function displayGames(games) {
+          const gameList = document.getElementById("game-list");
+          gameList.innerHTML = "";
+  
+          games.forEach((game) => {
+            const gameCard = document.createElement("div");
+            gameCard.classList.add("game-card");
+            gameCard.innerHTML = `
+              <img src="${game.image}" alt="${game.name}">
+              <h2>${game.name}</h2>
+              <p>Rating: ${game.rating}</p>
+            `;
+  
+            gameCard.addEventListener("mouseover", () => {
+              gameCard.style.backgroundColor = "grey";
+            });
+  
+            gameCard.addEventListener("mouseout", () => {
+              gameCard.style.backgroundColor = "";
+            });
+  
+            gameList.appendChild(gameCard);
+          });
+        }
+  
+        displayGames(gamesData);
+  
+        const darkModeButton = document.getElementById("darkModeButton");
+        const darkModeStyles = document.getElementById("darkModeStyles");
+        let darkMode = false;
+  
+        darkModeButton.addEventListener("click", () => {
+          darkMode = !darkMode;
+          const body = document.body;
+          if (darkMode) {
+            body.classList.add("dark-mode");
+            darkModeStyles.removeAttribute("disabled");
+          } else {
+            body.classList.remove("dark-mode");
+            darkModeStyles.setAttribute("disabled", "true");
           }
-
-          
-          displayGames(gamesData);
-
-          
-          const darkModeButton = document.getElementById("darkModeButton");
-          darkModeButton.addEventListener("click", () => {
-              
-              darkMode = !darkMode;
-
-              
-              const body = document.body;
-              if (darkMode) {
-                  body.classList.add("dark-mode");
-              } else {
-                  body.classList.remove("dark-mode");
-              }
-          });
-
-          
-          const filterButton = document.getElementById("filterButton");
-          filterButton.addEventListener("click", () => {
-              const ratingSelect = document.getElementById("ratingSelect");
-              const selectedRating = ratingSelect.value;
-
-              if (selectedRating === "all") {
-                  
-                  displayGames(gamesData);
-              } else {
-                  
-                  const filteredGames = gamesData.filter(game => game.rating == selectedRating);
-                  displayGames(filteredGames);
-              }
-          });
+        });
+  
+        const filterButton = document.getElementById("filterButton");
+        filterButton.addEventListener("click", () => {
+          const ratingSelect = document.getElementById("ratingSelect");
+          const selectedRating = ratingSelect.value;
+  
+          if (selectedRating === "all") {
+            displayGames(gamesData);
+          } else {
+            const filteredGames = gamesData.filter(game => game.rating == selectedRating);
+            displayGames(filteredGames);
+          }
+        });
       })
       .catch(error => {
-          console.error('Error fetching data:', error);
+        console.error('Error fetching data:', error);
       });
-});
+  });
+  
 
